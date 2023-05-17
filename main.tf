c544607849e8c16ecff476b533b880e4490af821
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-17 08:10:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+# main.tf
+
+variable "stage" {
+  type    = string
+  default = "dev"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+
+  name                 = "${var.stage}-vpc"
+  cidr                 = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Other VPC configuration parameters...
+}
 
