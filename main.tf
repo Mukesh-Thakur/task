4ee8082daa1fa8f5983309e0fe03066d4d48b19e
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-17 08:23:40 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,15 @@ variable "stage" {
   default = "dev"
 }
 
+variable "vpc_flag" {
+  type    = bool
+  default = false
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.0.0"
+  count   = var.vpc_flag ? 1 : 0
 
   name                 = "${var.stage}-vpc"
   cidr                 = "10.0.0.0/16"
