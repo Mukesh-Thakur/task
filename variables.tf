88fa2c08f246fc7094cf3fbdda9978e49e370f58
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-18 08:14:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,38 @@
 # variables.tf
 
+# Configure provider (AWS in this example)
+provider "aws" {
+  region = "us-east-1"
+} 
+
 variable "stage" {
   type    = string
   default = "dev"
 }
 
-variable "vpc_flag" {
+# Define variables
+variable "vpc_create" {
   type    = bool
   default = false
 }
-# Add other variables as needed...
+
+variable "vpc_name" {
+  type    = string
+  default = "my-vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
 
