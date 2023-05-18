3bba7dd83364a9bce3bacf84df1ad3abf9491a7c
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-18 21:07:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,151 +1,129 @@
-# main.tf
-# Create VPC module if vpc_create flag is true
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  # Conditionally create the VPC module based on the vpc_create flag
-  count  = var.vpc_create ? 1 : 0
+# variables.tf
 
-  name                 = "${var.stage}-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = ["us-east-1a", "us-east-1b"]
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
-  create_public_subnet = true
-  create_private_subnet = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_vpn_gateway   = false
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# Configure provider (AWS in this example)
+provider "aws" {
+  region = "us-east-1"
 }
-# Create public security group
-module "public_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
 
-  name        = "${var.stage}-public-sg"
-  description = "Public security group"
-  vpc_id      = var.vpc_id
+variable "stage" {
+  type    = string
+  default = "dev"
+}
 
-  ingress_rules = [
+# Define variables
+variable "vpc_create" {
+  type    = bool
+  default = true
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "my-vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+################################################################################
+# Private Network ACLs
+################################################################################
+
+variable "private_dedicated_network_acl" {
+  description = "Whether to use dedicated network ACL (not default) and custom rules for private subnets"
+  type        = bool
+  default     = false
+}
+
+variable "private_inbound_acl_rules" {
+  description = "Private subnets inbound network ACLs"
+  type        = list(map(string))
+  default = [
     {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    # Add more ingress rules as needed
-  ]
-
-  egress_rules = [
-    {
+      rule_number = 100
+      rule_action = "allow"
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_block  = "0.0.0.0/0"
     },
-    # Add more egress rules as needed
   ]
 }
 
-# Create private security group
-module "private_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.0.0"
-
-  name        = "${var.stage}-private-sg"
-  description = "Private security group"
-  vpc_id      = var.vpc_id
-
-  ingress_rules = [
+variable "private_outbound_acl_rules" {
+  description = "Private subnets outbound network ACLs"
+  type        = list(map(string))
+  default = [
     {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]  # Replace with the CIDR block of your VPC
-    },
-    # Add more ingress rules as needed
-  ]
-
-  egress_rules = [
-    {
+      rule_number = 100
+      rule_action = "allow"
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_block  = "0.0.0.0/0"
     },
-    # Add more egress rules as needed
   ]
 }
 
-# Create public network ACL
-module "public_nacl" {
-  source  = "terraform-aws-modules/acl/aws"
-  version = "2.1.0"
+variable "private_acl_tags" {
+  description = "Additional tags for the private subnets network ACL"
+  type        = map(string)
+  default     = {}
+}
 
-  name        = "${var.stage}-public-nacl"
-  vpc_id      = var.vpc_id
-  subnet_ids  = []  # Add the IDs of public subnets associated with this network ACL
+################################################################################
+# Public Network ACLs
+################################################################################
 
-  # Ingress rules
-  ingress = [
+variable "public_dedicated_network_acl" {
+  description = "Whether to use dedicated network ACL (not default) and custom rules for public subnets"
+  type        = bool
+  default     = false
+}
+
+variable "public_inbound_acl_rules" {
+  description = "Public subnets inbound network ACLs"
+  type        = list(map(string))
+  default = [
     {
-      rule_number   = 100
-      protocol      = "tcp"
-      rule_action   = "allow"
-      cidr_block    = "0.0.0.0/0"
-      from_port     = 80
-      to_port       = 80
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
     },
-    # Add more ingress rules as needed
-  ]
-
-  # Egress rules
-  egress = [
-    {
-      rule_number   = 100
-      protocol      = "tcp"
-      rule_action   = "allow"
-      cidr_block    = "0.0.0.0/0"
-      from_port     = 0
-      to_port       = 65535
-    },
-    # Add more egress rules as needed
   ]
 }
 
-# Create private network ACL
-module "private_nacl" {
-  source  = "terraform-aws-modules/acl/aws"
-  version = "2.1.0"
-
-  name        = "${var.stage}-private-nacl"
-  vpc_id      = var.vpc_id
-  subnet_ids  = []  # Add the IDs of private subnets associated with this network ACL
-
-  # Ingress rules
-  ingress = [
+variable "public_outbound_acl_rules" {
+  description = "Public subnets outbound network ACLs"
+  type        = list(map(string))
+  default = [
     {
-      rule_number   = 100
-      protocol      = "tcp"
-      rule_action   = "allow"
-      cidr_block    = "10.0.0.0/16"  # Replace with the CIDR block of your VPC
-      from_port     = 22
-      to_port       = 22
+      rule_number = 100
+      rule_action = "allow"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_block  = "0.0.0.0/0"
     },
-    # Add more ingress rules as needed
   ]
+}
 
-  # Egress rules
-  egress = [
-    {
-      rule_number   = 100
-      protocol      = "tcp"
-      rule_action   = "allow"
-      cidr_block    = "0.0.0.0/0"
-      from_port     = 0
-      to_port       = 65535
-    },
-    # Add more egress rules as needed
-  ]
+variable "public_acl_tags" {
+  description = "Additional tags for the public subnets network ACL"
+  type        = map(string)
+  default     = {}
 }
