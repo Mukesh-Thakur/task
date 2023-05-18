613624cce85bccbb49c87ea7d8de47bf1f6a2ebe
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-18 08:11:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,70 @@
 # main.tf
-
-variable "stage" {
-  type    = string
-  default = "dev"
-}
-
-variable "vpc_flag" {
-  type    = bool
-  default = false
-}
-
-variable "cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
+# Create VPC module if vpc_create flag is true
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
-  count   = var.vpc_flag ? 1 : 0
+  source = "terraform-aws-modules/vpc/aws"
+  # Conditionally create the VPC module based on the vpc_create flag
+  count  = var.vpc_create ? 1 : 0
 
-  name                 = "${var.stage}-vpc"
-  cidr                 = "10.0.0.0/16"
-# cidr                 = var.cidr
+  name                 = var.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = ["us-east-1a", "us-east-1b"]
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_vpn_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  # Other VPC configuration parameters...
 }
 
+# Create security group for public instances
+resource "aws_security_group" "public_sg" {
+  count = var.vpc_create ? 1 : 0
+
+  name        = "public-sg"
+  description = "Security group for public instances"
+  vpc_id      = module.vpc[0].vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create security group for private instances
+resource "aws_security_group" "private_sg" {
+  count = var.vpc_create ? 1 : 0
+
+  name        = "private-sg"
+  description = "Security group for private instances"
+  vpc_id      = module.vpc[0].vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create network ACLs
+resource "aws_network_acl" "public_nacl" {
+  count = var.vpc_create ? 1 : 0
+
+  vpc_id = module.vpc[0].vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    rule_action = "allow"
+    cidr_block  = "0
