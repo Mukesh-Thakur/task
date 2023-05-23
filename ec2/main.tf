e2ccd38b6d1bda150e0f426dade48d71558a094e
Mukesh-Thakur <60689704+Mukesh-Thakur@users.noreply.github.com>
2023-05-23 09:24:21 +0530

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,116 @@
+# to create s3 bucket
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  versioning = {
+    enabled = true
+  }
+}
+
+# To create ec2 instance
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "${var.stage}-ec2"
+
+  instance_type          = "t2.micro"
+  ami                    = var.ec2_instance_ami
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4" #module.vpc.private_subnets
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# To create and map EIP with ec2
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+resource "aws_eip_association" "example" {
+  instance_id   = module.ec2_instance.id[0]
+  allocation_id = aws_eip.example.id
+}
+
+# To create ASG and map EBS to ec2
+module "asg" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  name   = var.asg_name
+  create = true
+
+  # Launch configuration
+  #launch_template = module.ec2_instance.launch_template.name
+  launch_template_name        = var.launch_template_name
+  create_launch_template      = true
+  image_id                    = var.ec2_instance_ami
+  instance_type               = var.ec2_instance_type
+  instance_name               = var.ec2_instance_name
+  security_groups             = [var.ec2_sg]
+  iam_role_name               = "example-role"
+  create_iam_instance_profile = true
+  enable_monitoring           = true
+  #key_name                   = var.key_name
+  #iam_instance_profile_arn   = aws_iam_instance_profile.ec2-instance.arn
+  #user_data_base64           = base64encode(local.user_data)
+
+
+
+  # Auto scaling group
+  vpc_zone_identifier       = [var.public_subnet_ids]
+  health_check_type         = "EC2"
+  min_size                  = var.asg_min_size
+  max_size                  = var.asg_max_size
+  desired_capacity          = var.asg_desired_capacity
+  wait_for_capacity_timeout = 0
+
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+      }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  scaling_policies = {
+    my-policy = {
+      policy_type = "TargetTrackingScaling"
+      target_tracking_configuration = {
+        predefined_metric_specification = {
+          predefined_metric_type = "ASGAverageCPUUtilization"
+          resource_label         = "MyLabel"
+        }
+        target_value = 50.0
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "megasecret"
+  }
+}
